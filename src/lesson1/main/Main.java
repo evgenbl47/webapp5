3935d6957d90d45206a757f18476a4db4fe641d0
evgenbl47 <evgenbl47@gmail.com>
2020-01-19 02:32:07 +0500

    First commit. Add to simple signature.

---
+++ src/lesson1/main/Main.java
@@ -0,0 +1,10 @@
+package lesson1.main;
+
+/**
+ * Created by EvGen on 19.01.2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
